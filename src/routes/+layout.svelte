020baeda9bc5db0e53491cba3db5a891cd11127d
andrew <andrew@housermail.com>
2025-05-02 18:06:27 -0400

    added fancy loading bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,18 @@
 <script lang="ts">
 	import '../app.css';
-
-	/** add loading bar between page loads
+	import 'nprogress/nprogress.css';
 	import NProgress from 'nprogress';
-	import { beforeNavigate, afterNavigate } from '$app/navigation';
+	import { afterNavigate, beforeNavigate, goto } from '$app/navigation';
 
 	NProgress.configure({ showSpinner: false });
 
-	
-	beforeNavigate(() => {
+	beforeNavigate(async () => {
 		NProgress.start();
 	});
 
-	afterNavigate(() => {
+	afterNavigate(async () => {
 		NProgress.done();
 	});
-	*/
 
 	let { children } = $props();
 </script>
